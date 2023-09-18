52b42ebfcd77d26075d04476b1ac6ea6394b7bd5
Manomay Kagalkar <manomay111@gmail.com>
2023-09-18 11:45:19 +0530

    updated profile page styling

--- nurtureapp/app/profile/page.tsx
+++ nurtureapp/app/profile/page.tsx
@@ -15,8 +15,14 @@ export default function Page() {
       <section className="h-screen w-screen">
         <Header />
         <nav className="flex w-screen h-max p-3 flex-row justify-around items-center border-b-2 border-black">
-          <Image width={100} height={100} src={session.user.image} alt={session.user.name} className="rounded-full w-12 h-12" />
-          <h1 className="text-bold text-center text">{session.user.name}</h1>
+          <Image width={100} height={100} src={session.user.image} alt={session.user.name} className="rounded-full w-10 lg:w-12 h-10 lg:h-12" />
+          <h1 className="text-bold text-center text">
+            {
+              session.user.name
+                .substring(0, session.user.name.indexOf(' '))
+                .toLowerCase()
+            }
+          </h1>
           <SignOutButton styleClass={`text-red-500 text-center hover:text-white hover:scale-[1.01] duration-100 border-2 border-red-400 hover:border-black bg-white hover:bg-red-500 p-1 px-3 rounded-md hover:rounded-lg`} />
         </nav>
         <main className="flex flex-col h-[400px] w-screen justify-center items-center gap-10 p-14 text-center">
