94174a880b6724fa77a28c2d928becb1e498c182
Pranav Chunchur <112144655+PChunchur@users.noreply.github.com>
2023-12-07 23:03:54 +0530

    Initial Commit

---
+++ nurtureiOS/Nurture/NurtureApp.swift
@@ -0,0 +1,17 @@
+//
+//  NurtureApp.swift
+//  Nurture
+//
+//  Created by Pranav Chunchur on 06/11/23.
+//
+
+import SwiftUI
+
+@main
+struct NurtureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
