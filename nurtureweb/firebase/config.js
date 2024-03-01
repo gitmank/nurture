38a0f485b280f74988f2577e6943236afef207fb
Manomay Kagalkar <manomay111@gmail.com>
2024-03-02 00:00:12 +0530

    added firebase packages and config

---
+++ nurtureweb/firebase/config.js
@@ -0,0 +1,17 @@
+// import firebase app from SDK
+import { initializeApp } from "firebase/app";
+
+// firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
+
+// initialize firebase app
+const app = initializeApp(firebaseConfig);
+
+export default app;
