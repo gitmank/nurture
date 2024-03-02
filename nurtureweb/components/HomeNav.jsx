57b19c36dd13f58faa2ff116ea1a11786b6b1c39
Manomay Kagalkar <manomay111@gmail.com>
2024-03-02 14:36:50 +0530

    added navigation components

---
+++ nurtureweb/components/HomeNav.jsx
@@ -0,0 +1,30 @@
+import Link from "next/link"
+
+// add or remove navigation links to this array
+const navlinks = [
+    {
+        name: '🔑 Login',
+        href: '/signin',
+    },
+    {
+        name: '🏡 Dashboard',
+        href: '/dashboard',
+    },
+]
+
+export default function HomeNav() {
+
+    return (
+        <nav className="flex flex-row w-screen h-[100px] items-center justify-around bg-black text-white">
+            {
+                navlinks.map((link, index) => {
+                    return (
+                        <Link className="hover:border-white border-2 p-3 duration-100 rounded-xl border-black" href={link.href} key={index}>
+                            {link.name}
+                        </Link>
+                    )
+                })
+            }
+        </nav>
+    )
+}
